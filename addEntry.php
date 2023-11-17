b0172c3d43df64185e878243fc844a0d922723af
Yilian Zhao <yilian27@students.cs.ubc.ca>
2023-11-16 19:57:13 -0800

    insert operation on Monster table

--- addEntry.php
+++ addEntry.php
@@ -14,161 +14,48 @@
             <form>
                 <label for="addComponents">Component</label>
                 <select id="addComponents" name="addComponents">
+                    <option value="Monster">Monster</option>
                     <option value="PlayableCharacter">PlayableCharacter</option>
                     <option value="Equipment">Equipment</option>
                     <option value="Quest">Quest</option>
                     <option value="NPC">NPC</option>
                     <option value="Village">Village</option>
-                    <option value="Monster">Monster</option>
                     <option value="Dungeon">Dungeon</option>
                   </select>
               </form>
         </div>
         <div>
-            <h2>Reset</h2>
-            <p>If you wish to reset the tables press on the reset button</p>
-
-            <form method="POST" action="addEntry.php" class="attributes">
-                <input type="hidden" id="resetTablesRequest" name="resetTablesRequest">
-                <p><input type="submit" value="Reset" name="reset"></p>
+            <h2>Setup</h2>
+            <p>This button is for setting up the tables</p>
+            <form method="POST" action="addEntry.php" >
+                <input type="hidden" id="setupTablesRequest" name="setupTablesRequest">
+                <p><input type="submit" value="Setup" name="setup" class="buttons"></p>
             </form>
         </div>
 
-        <div>
-            <h2>Populate Tables</h2>
-            <p>If you wish to populate the tables press on the populate button</p>
 
-            <form method="POST" action="addEntry.php" class="attributes">
-                <input type="hidden" id="populateTablesRequest" name="populateTablesRequest">
-                <p><input type="submit" value="Populate" name="populateSubmit"></p>
-            </form>
-        </div>
 
         <div id="addAttributes">
-            <form class="attributes activeTable" id="pcAdd">
-                <fieldset>
-                    <label for="username">username</label>
-                    <input type="text" name="username" id="addUsername"><br>
-                    <label for="class">class</label>
-                    <input type="text" name="class" id="addClass"><br>
-                    <label for="charLevel">level</label>
-                    <input type="text" name="charLevel" id="addCharLevel"><br>
-                    <label for="health">health</label>
-                    <input type="text" name="health" id="addHealth"><br>
-                    <label for="energy">energy</label>
-                    <input type="text" name="energy" id="addEnergy"><br>
-                    <label for="attack">attack</label>
-                    <input type="text" name="attack" id="addAttack"><br>
-                    <label for="defense">defense</label>
-                    <input type="text" name="defense" id="addDefense"><br>
-                    <label for="speed">speed</label>
-                    <input type="text" name="speed" id="addSpeed"><br>
-                    <label for="pet">pet</label>
-                    <input type="text" name="pet" id="addPet"><br>
-                    <input type="submit" value="add entry">
-                </fieldset>
+            <h2>Insert Values into Monster</h2>
+            <form method="POST" action="addEntry.php" class="attribute" id="villageAttributes"> <!--refresh page when submitted-->
+            <input type="hidden" id="insertQueryRequest" name="insertQueryRequest">
+            Name: <input type="text" name="monsName"> <br /><br />
+            Type: <input type="text" name="monsType"> <br /><br />
+            Level: <input type="text" name="monsLevel"> <br /><br />
+            Health: <input type="text" name="monsHealth"> <br /><br />
+            Attack: <input type="text" name="monsAttack"> <br /><br />
+            Defense: <input type="text" name="monsDefense"> <br /><br />
+            Defends: <input type="text" name="defends"> <br /><br />
+
+            <input type="submit" value="Insert" name="insertSubmit"></p>
             </form>
-            <!-- uses EquipmentName x EquipmentUser x EquipmentStatBoost x EquipmentType table -->
-            <form class="attributes" id="equipmentAdd">
-                <fieldset>
-                    <label for="equipName">name</label>
-                    <input type="text" name="equipName" id="addEquipName"><br>
-                    <label for="equipType">type</label>
-                    <input type="text" name="equipType" id="addEquipType"><br>
-                    <label for="rarity">rarity</label>
-                    <input type="text" name="rarity" id="addRarity"><br>
-                    <label for="affectedStat">affectedStat</label>
-                    <input type="text" name="affectedStat" id="addAffectedStat"><br>
-                    <label for="statBoost">statBoost</label>
-                    <input type="text" name="statBoost" id="addStatBoost"><br>
-                    <label for="usedBy">usedBy</label>
-                    <input type="text" name="usedBy" id="addUsedBy"><br>
-                    <input type="submit" value="add entry">
-                </fieldset>
-            </form>
-            <form class="attributes" id="questAdd">
-                <fieldset>
-                    <label for="questTitle">title</label>
-                    <input type="text" name="questTitle" id="addQuestTitle"><br>
-                    <label for="difficulty">difficulty</label>
-                    <input type="text" name="difficulty" id="addDifficulty"><br>
-                    <label for="reward">reward</label>
-                    <input type="text" name="reward" id="reward"><br>
-                    <label for="questLength">length</label>
-                    <input type="text" name="questLength" id="addQuestLength"><br>
-                    <label for="questMinLevel">minLevel</label>
-                    <input type="text" name="questMinLevel" id="addQuestMinLevel"><br>
-                    <label for="startNPC">startNPC</label>
-                    <input type="text" name="startNPC" id="addStartNPC"><br>
-                    <input type="submit" value="add entry">
-                </fieldset>
-            </form>
-            <form class="attributes" id="npcAdd">
-                <fieldset>
-                    <label for="npcName">name</label>
-                    <input type="text" name="npcName" id="addNpcName"><br>
-                    <label for="npcTitle">title</label>
-                    <input type="text" name="npcTitle" id="addNpcTitle"><br>
-                    <label for="npcVillage">village</label>
-                    <input type="text" name="npcVillage" id="addNpcVillage"><br>
-                    <input type="submit" value="add entry">
-                </fieldset>
-            </form>
-            <h2>Village</h2>
-            <form method="POST" action="addEntry.php" class="attributes" id="villageAdd">
-                <input type="hidden" id="insertQueryRequest" name="insertQueryRequest">
-                <fieldset>
-                    <label for="villageName">name</label>
-                    <input type="text" name="villageName" id="addVillageName"><br>
-                    <label for="region">region</label>
-                    <input type="text" name="region" id="addRegion"><br>
-                    <label for="population">population</label>
-                    <input type="text" name="population" id="addPopulation"><br>
-                    <label for="villageMinLevel">minLevel</label>
-                    <input type="text" name="villageMinLevel" id="addVillageMinLevel"><br>
-                    <input type="submit" value="add entry" name=insertSubmit>
-                </fieldset>
-            </form>
-            <form method="GET" action="addEntry.php" class="attributes"> <!--refresh page when submitted-->
+
+
+        <h2>Count the Tuples in Monster</h2>
+        <form method="GET" action="addEntry.php"> <!--refresh page when submitted-->
             <input type="hidden" id="countTupleRequest" name="countTupleRequest">
-            <input type="submit" value = "Count Tuples" name="countTuples">
+            <input type="submit" name="countTuples" class="buttons" value="Count"></p>
         </form>
-            <form class="attributes" id="monsterAdd">
-                <fieldset>
-                    <label for="monsName">name</label>
-                    <input type="text" name="monsName" id="addMonsName"><br>
-                    <label for="monsType">type</label>
-                    <input type="text" name="monsType" id="addMonsType"><br>
-                    <label for="monsLevel">level</label>
-                    <input type="text" name="monsLevel" id="addMonsLevel"><br>
-                    <label for="monsHealth">health</label>
-                    <input type="text" name="monsHealth" id="addMonsHealth"><br>
-                    <label for="monsAttack">attack</label>
-                    <input type="text" name="monsAttack" id="addMonsAttack"><br>
-                    <label for="monsDefense">defense</label>
-                    <input type="text" name="monsDefense" id="addMonsDefense"><br>
-                    <label for="defends">defends</label>
-                    <input type="text" name="defends" id="addDefends"><br>
-                    <input type="submit" value="add entry">
-                </fieldset>
-            </form>
-            <!-- based on DungeonName x DungeonMinLevelToDifficulty x DungeonRegion table -->
-            <form class="attributes" id="dungeonAdd">
-                <fieldset>
-                    <label for="dungName">name</label>
-                    <input type="text" name="dungName" id="addDungName"><br>
-                    <label for="dungDifficulty">difficulty</label>
-                    <input type="text" name="dungDifficulty" id="addDungDifficulty"><br>
-                    <label for="dungMinLevel">minLevel</label>
-                    <input type="text" name="dungMinLevel" id="addDungMinLevel"><br>
-                    <label for="dungRegion">region</label>
-                    <input type="text" name="dungRegion" id="addDungRegion"><br>
-                    <label for="charLevel">boss</label>
-                    <input type="text" name="boss" id="addBoss"><br>
-                    <input type="submit" value="add entry">
-                </fieldset>
-            </form>
-        </div>
 
         <a href="index.html" class="backButton">back</a>
 
@@ -240,12 +127,16 @@
 				}
 
                 $r = OCIExecute($statement, OCI_DEFAULT);
+
                 if (!$r) {
-                    echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-                    $e = OCI_Error($statement); // For OCIExecute errors, pass the statementhandle
-                    echo htmlentities($e['message']);
-                    echo "<br>";
+                    echo "Sorry, an error occurred";
+                    // echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+                    // $e = OCI_Error($statement); // For OCIExecute errors, pass the statementhandle
+                    // echo htmlentities($e['message']);
+                    // echo "<br>";
                     $success = False;
+                } else {
+                    echo "Success!";
                 }
             }
         }
@@ -298,7 +189,7 @@
             OCICommit($db_conn);
         }
 
-        function handleResetRequest() {
+        function handleSetupRequest() {
             global $db_conn;
             // Drop old data
             $clear = file_get_contents('clear.sql');
@@ -308,7 +199,7 @@
                 executePlainSQL($sqlRows[$x]);
               }
             //run create SQL file
-            $myfile = file_get_contents('create.sql');
+            $myfile = file_get_contents('setup.sql');
             $sqlRows=explode(";",$myfile);
             $len = count($sqlRows);
             for ($x = 0; $x < $len; $x++) {
@@ -323,27 +214,29 @@
 
             //Getting the values from user and insert data into the table
             $tuple = array (
-                ":bind1" => $_POST['villageName'],
-                ":bind2" => $_POST['region'],
-                ":bind3" => $_POST['population'],
-                ":bind4" => $_POST['villageMinLevel']
+                ":bind1" => $_POST['monsName'],
+                ":bind2" => $_POST['monsType'],
+                ":bind3" => $_POST['monsLevel'],
+                ":bind4" => $_POST['monsHealth'],
+                ":bind5" => $_POST['monsAttack'],
+                ":bind6" => $_POST['monsDefense'],
+                ":bind7" => $_POST['defends']
             );
 
             $alltuples = array (
                 $tuple
             );
-
-            executeBoundSQL("insert into Village values (:bind1, :bind2, :bind3, :bind4)", $alltuples);
+            executeBoundSQL("insert into Monster values (:bind1, :bind2, :bind3, :bind4, :bind5, :bind6, :bind7)", $alltuples);
             OCICommit($db_conn);
         }
 
         function handleCountRequest() {
             global $db_conn;
 
-            $result = executePlainSQL("SELECT Count(*) FROM Village");
+            $result = executePlainSQL("SELECT Count(*) FROM Monster");
 
             if (($row = oci_fetch_row($result)) != false) {
-                echo "<br> The number of tuples in Village: " . $row[0] . "<br>";
+                echo "<br> The number of tuples in Monster: " . $row[0] . "<br>";
             }
         }
 
@@ -363,8 +256,8 @@
 	// A better coding practice is to have one method that reroutes your requests accordingly. It will make it easier to add/remove functionality.
         function handlePOSTRequest() {
             if (connectToDB()) {
-                if (array_key_exists('resetTablesRequest', $_POST)) {
-                    handleResetRequest();
+                if (array_key_exists('setupTablesRequest', $_POST)) {
+                    handleSetupRequest();
                 } else if (array_key_exists('updateQueryRequest', $_POST)) {
                     handleUpdateRequest();
                 } else if (array_key_exists('insertQueryRequest', $_POST)) {
@@ -389,7 +282,7 @@
             }
         }
 
-		if (isset($_POST['reset']) || isset($_POST['updateSubmit']) || isset($_POST['insertSubmit']) || isset($_POST['populateSubmit'])) {
+		if (isset($_POST['setup']) || isset($_POST['updateSubmit']) || isset($_POST['insertSubmit'])) {
             handlePOSTRequest();
         } else if (isset($_GET['countTupleRequest'])) {
             handleGETRequest();
